2f3a72171d5afe5a72e53c864fcd39d1606244ae
Athens <athens314@hotmail.com>
2018-01-22 11:43:43 -0500

    clear master branch

--- src/com/athensoft/ecomm/dao/item/ItemSpecDao.java
+++
@@ -1,9 +0,0 @@
-package com.athensoft.ecomm.dao.item;
-
-import java.util.List;
-
-import com.athensoft.ecomm.entity.item.ItemSpec;
-
-public interface ItemSpecDao {
-	public List<ItemSpec> findItemSpecByItemId(int itemId);
-}
--- src/com/athensoft/ecomm/dao/sale/ItemSaleFeaturedDao.java
+++
@@ -1,9 +0,0 @@
-package com.athensoft.ecomm.dao.sale;
-
-import java.util.List;
-
-import com.athensoft.ecomm.entity.sale.ItemSaleFeatured;
-
-public interface ItemSaleFeaturedDao {
-	public List<ItemSaleFeatured> getFeaturedItem();
-}
